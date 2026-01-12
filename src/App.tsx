3f3b75b4bfa27c878fe9391004ffb73a8f74c2f4
Andre <andrezicadobaile1@gmail.com>
2026-01-12 12:03:27 -0300

    Adicionando Container ao Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
- import { TimerIcon } from 'lucide-react';
+
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
  
- import './styles/global.css';
- import './styles/theme.css';
+import './styles/global.css';
+import './styles/theme.css';
 
  export function App() {
     
     return( 
     <>
-    <Heading>
-         Olá Mundo!
-         
-    <button>
-        <TimerIcon/>
-    </button>
-         
-    </Heading>
+    <Container>
+            <Heading>
+                Logo
+            </Heading>
+          </Container>
 
-    <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit dignissimos id ipsam repellendus dolore veritatis recusandae, quibusdam obcaecati? Dolorem cum vel harum dolor voluptate omnis, autem est non animi delectus!
-    </p>
+        <Container>
+            <Heading>
+                Menu
+            </Heading>
+          </Container>
+
+       
     </>
     );
 }
