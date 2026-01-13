ea9ebe7b8edb0d8b36dfa4cea2ac7a4895a8c013
Andre <andrezicadobaile1@gmail.com>
2026-01-13 11:07:56 -0300

    Adicionando Logo ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
  
 import './styles/global.css';
 import './styles/theme.css';
@@ -10,10 +11,8 @@ import './styles/theme.css';
     return( 
     <>
     <Container>
-            <Heading>
-                Logo
-            </Heading>
-          </Container>
+        <Logo/>
+        </Container>
 
         <Container>
             <Heading>
