86e856c44b38875326f9a4a670ee19ade60f1f9c
Andre <andrezicadobaile1@gmail.com>
2026-01-14 15:28:29 -0300

    Arrumando o lugar das tasks

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { CountDown } from './components/CountDown';
 
 import './styles/global.css';
 import './styles/theme.css';
+import { DefaultInput } from './components/DefaultInput';
 
  export function App() {
     
@@ -21,6 +22,28 @@ import './styles/theme.css';
 
         <Container>
             <CountDown/>
+          </Container>
+
+          <Container>
+            <form className='form' action="">
+                <div className='formRow'>
+                    <DefaultInput  labelText= "task" id="meuInput" type="text" placeholder='Digite Algo'/>
+                </div>
+
+                    <div className='formRow'>
+                   <p>Lorem ipsum dolor sit amet.</p>
+                 </div>
+
+                 
+                    <div className='formRow'>
+                   <p>Ciclos</p>
+                   <p>0 0 0 0 0</p>
+                 </div>
+
+                   <div className='formRow'>
+                   <button>Enviar</button>
+                 </div>
+            </form>
           </Container>
        
     </>
