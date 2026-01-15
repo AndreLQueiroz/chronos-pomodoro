78b76914e45f1d1f54fb86a729a077ca3fa5bffa
Andre <andrezicadobaile1@gmail.com>
2026-01-15 19:33:10 -0300

    Adicinando Page para o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
 
 import './styles/global.css';
 import './styles/theme.css';
-import { DefaultInput } from './components/DefaultInput';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
+
+
 
  export function App() {
     
@@ -34,16 +40,18 @@ import { DefaultInput } from './components/DefaultInput';
                    <p>Lorem ipsum dolor sit amet.</p>
                  </div>
 
-                 
                     <div className='formRow'>
-                   <p>Ciclos</p>
-                   <p>0 0 0 0 0</p>
+                   <Cycles/>
                  </div>
 
                    <div className='formRow'>
-                   <button>Enviar</button>
+                   <DefaultButton icon={<PlayCircleIcon/>} color="green"/>
                  </div>
             </form>
+          </Container>
+
+          <Container>
+            <Footer/>
           </Container>
        
     </>
